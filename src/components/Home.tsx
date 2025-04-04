2e186fc02f04559ec9b8c0f3be83a38f1f75e9ee
Tommy Luu <tommydl@stud.ntnu.no>
2025-04-04 18:17:17 +0200

    Refactor Home component to simplify error and loading messages rendering

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,19 +9,11 @@ const Home = () => {
   const { data: items, error } = useSWR<Item[]>(`${backendUrl}/item`, fetcher);
   if (error) {
     console.log("Error fetching items");
-    return (
-      <div className="flex justify-center pt-16 text-neutral-100">
-        Error fetching items.
-      </div>
-    );
+    return <div className="flex justify-center pt-16 text-neutral-100">Error fetching items.</div>;
   }
   if (!items) {
     console.log("Loading items...");
-    return (
-      <p className="flex justify-center items-center h-screen text-gray-500">
-        Loading...
-      </p>
-    );
+    return <p className="flex items-center justify-center h-screen text-gray-500">Loading...</p>;
   }
 
   return (
@@ -30,9 +22,7 @@ const Home = () => {
       {items.length > 0 ? (
         <ItemList items={items} />
       ) : (
-        <p className="flex justify-center items-center h-screen text-gray-500">
-          No items found
-        </p>
+        <p className="flex items-center justify-center h-screen text-gray-500">No items found</p>
       )}
     </div>
   );
